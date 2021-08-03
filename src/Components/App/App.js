535995ff71a0a090132c306b1c8d8529f3998bf7
VickyKR37 <67956865+VickyKR37@users.noreply.github.com>
2021-08-03 12:26:28 +0100

    iniital commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+// import './Playlist';
+// import './SearchResults';
+// import '../SearchBar/SearchBar.js';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+      
+        {/* <SearchBar /> */}
+        <div className="App-playlist">
+        
+        {/* <SearchResults /> */}
+          
+          {/* <Playlist /> */}
+        </div>
+      </div>
+      </div>
+    )
+  }
+} 
+
+export default App;

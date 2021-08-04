62b3c9b3e7dc9cfced5440e73c2202118a60033c
VickyKR37 <67956865+VickyKR37@users.noreply.github.com>
2021-08-04 11:35:32 +0100

    added functionality to hook up search bar to Spotify search

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -31,6 +31,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
     this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
   
   removeTrack(track) {
@@ -47,6 +48,10 @@ class App extends React.Component {
 
   savePlaylist() {
     const trackURIs = this.state.playlistTracks.map(track => track.uri);
+  }
+
+  search(searchTerm) {
+    console.log(searchTerm);
   }
 
   render() {
